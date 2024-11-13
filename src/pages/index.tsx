3182a3895e32e3a4b074773fb0a0a482918571f6
sky <kongyang@petalmail.com>
2024-11-13 17:50:26 +0800

    home page add doc button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,13 +16,13 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/* <div className={styles.buttons}>
+        <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/ubuntu22.04使用">
+            ZENUS Q 系列
           </Link>
-        </div> */}
+        </div>
       </div>
     </header>
   );
